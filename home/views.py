5310b65428767e97c12ebe5f4b64cd00914f0f40
SKKU-26\Skku <kimbumsoo2018@skkukdp.re.kr>
2024-09-25 16:45:18 +0900

    Modify detail html

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from home.models import *
 from home.serializers import *
 from rest_framework import mixins, viewsets
+
 
 # Create your views here.
 
@@ -19,6 +21,11 @@ def login(request):
 
 def signup(request):
     return render(request, "home/signup.html")
+
+
+def detail(request, pk):
+    context =  get_object_or_404(Game, id = pk)
+    return render(request, "home/<int:pk>.html", context)
 
 
 class GameViewSet(viewsets.ReadOnlyModelViewSet):
